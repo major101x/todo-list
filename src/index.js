a51cf5985a2d2348286f33dd094d5efaae9e18e1
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-12-02 00:45:28 +0100

    add test code to index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import "./styles.css";
+import { todoModule } from "./todoModule";
 
+todoModule.printTodos();
+todoModule.addTodo({
+  title: "do sished",
+  description: "just do the dishes",
+  dueDate: "tomorrow",
+  priority: "high",
+});
+todoModule.addTodo({
+  title: "mao",
+  description: "just do the dishes",
+  dueDate: "tomorrow",
+});
+todoModule.addTodo({
+  title: "sissy",
+  description: "just do the dishes",
+  dueDate: "today",
+});
+todoModule.editTodo("mao", "description", "hi");
+
+todoModule.removeTodo("sissy");
+todoModule.printTodos();
