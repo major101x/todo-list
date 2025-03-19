5b0c320637f9238ef99eb8cfd57e86e7f2512b8f
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2025-03-19 19:01:54 +0100

    add creation of todos functionality

--- src/components/modals.js
+++ src/components/modals.js
@@ -1,3 +1,7 @@
+import { projects } from "../index";
+import { projectDisplay } from "./projectDisplay";
+import { todoDisplay } from "./todoDisplay";
+
 const openModal = (modal) => {
   modal.showModal();
 };
@@ -10,6 +14,26 @@ export const addTaskModal = () => {
   const addTaskModalElem = document.querySelector("#add-task-modal");
   const addTaskBtn = document.querySelector(".add-task-btn");
   const closeAddTaskModalBtn = document.querySelector(".add-task-cancel");
+  const titleInput = document.querySelector("#task-title");
+  const descriptionInput = document.querySelector("#task-description");
+  const dueDateInput = document.querySelector("#due-date");
+  const taskPrioritySelect = document.querySelector("#task-priority");
+  const taskProjectSelect = document.querySelector("#task-project");
+  const confirmBtn = document.querySelector(".add-task-confirm");
+  const todoContainer = document.querySelector(".todo-container");
+  const allInputs = document.querySelectorAll("#add-task-modal input");
+
+  // Gets the project titles
+  const taskProjectSelectOptions = projects
+    .getProjects()
+    .map((project) => project.title);
+
+  // Creates a new select option for each project title
+  taskProjectSelectOptions.forEach((project) => {
+    const projectOption = document.createElement("option");
+    projectOption.textContent = project;
+    taskProjectSelect.appendChild(projectOption);
+  });
 
   // Opens the add task modal
   addTaskBtn.addEventListener("click", () => openModal(addTaskModalElem));
@@ -18,6 +42,27 @@ export const addTaskModal = () => {
   closeAddTaskModalBtn.addEventListener("click", () =>
     closeModal(addTaskModalElem)
   );
+
+  // Creates a new todo and re-renders the project display
+  confirmBtn.addEventListener("click", () => {
+    projects.addTodoToProject(taskProjectSelect.value, {
+      title: titleInput.value,
+      description: descriptionInput.value,
+      dueDate: dueDateInput.value,
+      priority: taskPrioritySelect.value,
+      checked: false,
+    });
+    projectDisplay(
+      taskProjectSelect.value,
+      projects.getTodosInProject(taskProjectSelect.value),
+      todoContainer
+    );
+    closeModal(addTaskModalElem);
+
+    // Reset all inputs
+    allInputs.forEach((input) => (input.value = ""));
+    taskPrioritySelect.value = "none";
+  });
 };
 
 export const addProjectModal = () => {
